e635f6545f2d99fe26b6fbb7a1732cf1aa994e3f
Kevin Khuu <kkhuu@uw.edu>
2022-03-29 23:36:19 -0700

    moveLeft, moveRight, moveUp, moveDown implemented

--- src/KeyListener.tsx
+++ src/KeyListener.tsx
@@ -44,8 +44,8 @@ class KeyListener extends Component<KeyListenerProps, KeyListenerState> {
                     }
 
                     // Combine numbers if they're the same.
-                    if(newBoard[k][j] === newBoard[i][j] && k !== i) {
-                        newBoard[k+1][j] *= newBoard[k+1][j];
+                    if(k+1 < 4 && k+1 !== i && newBoard[k+1][j] === newBoard[i][j]) {
+                        newBoard[k+1][j] = newBoard[k+1][j]*2;
                         newBoard[i][j] = 0;
                     }
                     else { // Just bring it to the first empty spot.
@@ -61,18 +61,93 @@ class KeyListener extends Component<KeyListenerProps, KeyListenerState> {
     }
 
     moveUp() {
-        console.log("moved up")
-        this.addNumber(this.props.board);
+        let newBoard = this.props.board;
+        for(let i = 1; i <= 3; i++) {
+            for(let j = 0; j < 4; j++) {
+                if(newBoard[i][j] !== 0) {
+                    let k = i;
+
+                    // find the highest empty space
+                    //We need to stop kevin's racism - I concur!
+                    while(k-1 !== -1 && newBoard[k-1][j] === 0) {
+                        k--;
+                    }
+
+                    // Combine numbers if they're the same.
+                    if(k-1 > -1 && k-1 !== i && newBoard[k-1][j] === newBoard[i][j]) {
+                        newBoard[k-1][j] = newBoard[k-1][j]*2;
+                        newBoard[i][j] = 0;
+                    }
+                    else { // Just bring it to the first empty spot.
+                        if(k != i) {
+                            newBoard[k][j] = newBoard[i][j];
+                            newBoard[i][j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        this.addNumber(newBoard);
     }
 
     moveLeft() {
-        console.log("moved left")
-        this.addNumber(this.props.board);
+        let newBoard = this.props.board;
+        for(let j = 1; j <= 3; j++) {
+            for(let i = 0; i < 4; i++) {
+                if(newBoard[i][j] !== 0) {
+                    let k = j;
+
+                    // find the highest empty space
+                    //We need to stop kevin's racism - I concur!
+                    while(k-1 !== -1 && newBoard[i][k-1] === 0) {
+                        k--;
+                    }
+
+                    // Combine numbers if they're the same.
+                    if(k-1 > -1 && k-1 !== j && newBoard[i][k-1] === newBoard[i][j]) {
+                        newBoard[i][k-1] = newBoard[i][k-1]*2;
+                        newBoard[i][j] = 0;
+                    }
+                    else { // Just bring it to the first empty spot.
+                        if(k != j) {
+                            newBoard[i][k] = newBoard[i][j];
+                            newBoard[i][j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        this.addNumber(newBoard);
     }
 
     moveRight() {
-        console.log("moved right")
-        this.addNumber(this.props.board);
+        let newBoard = this.props.board;
+        for(let j = 2; j >= 0; j--) {
+            for(let i = 0; i < 4; i++) {
+                if(newBoard[i][j] !== 0) {
+                    let k = j;
+
+                    // find the highest empty space
+                    //We need to stop kevin's racism - I concur!
+                    while(k+1 !== 4 && newBoard[i][k+1] === 0) {
+                        k++;
+                    }
+
+                    // Combine numbers if they're the same.
+                    if(k+1 < 4 && k+1 !== j && newBoard[i][k+1] === newBoard[i][j]) {
+                        newBoard[i][k+1] = newBoard[i][k+1]*2;
+                        newBoard[i][j] = 0;
+                    }
+                    else { // Just bring it to the first empty spot.
+                        if(k != j) {
+                            newBoard[i][k] = newBoard[i][j];
+                            newBoard[i][j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        this.addNumber(newBoard);
     }
 
     addNumber(newBoard: number[][]){
